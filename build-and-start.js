1a7677dfdb655e275993225f78e1c5bfc1b372b0
vs <valeriisklyarov@gmail.com>
2025-03-07 17:36:12 +0200

    fix(build): update default port to 4041 and load environment variables from .env.production

--- build-and-start.js
+++ build-and-start.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-console */
 const { execSync } = require('child_process');
 
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '.env.production') });
+
 try {
   console.log('Building the auth application...');
   execSync('npm run build', { stdio: 'inherit' });
   console.log('Starting the auth application...');
   
   // Get the PORT from environment variables or use a default
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4041;
   
   // Explicitly set the PORT environment variable when running npm start
   execSync(`PORT=${port} npm start`, { stdio: 'inherit' });
