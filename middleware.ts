09ff6da93c9c7296f001bdd203e56e896182c731
vs <valeriisklyarov@gmail.com>
2025-12-18 14:29:56 +0200

    fix(middleware): update matcher regex to include all API routes

--- middleware.ts
+++ middleware.ts
@@ -255,5 +255,5 @@ export default function middleware( req: NextRequest, event: NextPage) {
 }
 
 export const config = {
-matcher: ["/((?!api/lucia|api/auth|_next/static|_next/image|favicon.ico).*)"],
+matcher: ["/((?!api/|[a-z]{2}/api/|_next/static|_next/image|favicon.ico).*)"],
 }
